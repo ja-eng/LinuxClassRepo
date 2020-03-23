c61e61bd4a014d701638930b2e61fd3ffcb2b674
ja-eng <57498315+ja-eng@users.noreply.github.com>
2020-03-22 22:37:25 -0400

    Update myIfScript.sh

--- Homework/Week04/JacquelineRobinson/Problem1/myIfScript.sh
+++ Homework/Week04/JacquelineRobinson/Problem1/myIfScript.sh
@@ -1,1 +1,7 @@
+jacki@LAPTOP-EG61P3TC MINGW64 ~
+$ if test "b" = "b"; then echo "equal"; else echo "not equal"; fi
+equal
 
+jacki@LAPTOP-EG61P3TC MINGW64 ~
+$ if test "a" = "b"; then echo "equal"; else echo "not equal"; fi
+not equal
